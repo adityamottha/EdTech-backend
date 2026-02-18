39759b746b45e3b864f4209aef2cdbb71842bc94
adityamottha <adityamottha5@gmail.com>
2026-02-18 21:49:52 +0530

    Added multer middleware for file

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,16 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function(req,file,callback){
+        callback(null,"public/temp");
+    },
+
+    filename:function(req,file,callback){
+        const uniqueFileName = Date.now() + "_" +
+         Math.floor(Math.random()*100) +
+          "_" + file.originalname;
+          callback(null,uniqueFileName)
+    }
+});
+
+export const upload = multer({storage});
