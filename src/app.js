a055393a33936a67006dc79d151aeb7312d9b3df
adityamottha <adityamottha5@gmail.com>
2026-02-20 13:26:33 +0530

    Register boiler plate added

--- src/app.js
+++ src/app.js
@@ -16,6 +16,10 @@ app.use(express.static("public/temp"));
 app.use(express.urlencoded({limit:"16kb",extended:true}));
 app.use(cookieParser());
 
+// AUTH-ROUTER
+import  auth  from "./modules/userAuth/authUser.routes.js"
+app.use("/api/v1/users",auth);
+
 
 // ERROR MIDDLEWARE--------------------------------
 import { errorMiddleware } from "./middlewares/error.middleware.js";
--- src/modules/userAuth/authUser.controller.js
+++ src/modules/userAuth/authUser.controller.js
@@ -1,10 +1,14 @@
+import { ApiResponse } from "../../utils/ApiResponse.js"
 import { AsyncHandler } from "../../utils/AsyncHandler.js";
-import { registerUserService } from "./authUser.service.js";
+// import { registerUserService } from "./authUser.service.js";
 
-const registerUserController = AsyncHandler(async ()=>{
+const registerUserController = AsyncHandler(async (req,res)=>{
     // get a data from req.body
     // call the service function and pass parameter
     // return response
+    return res.status(200).json(
+        new ApiResponse(200,{},"User register successfully!")
+    )
 });
 
 export { registerUserController }
