cd2c1e983ae3429f3b3e0f48eb20e48459519e98
adityamottha <adityamottha5@gmail.com>
2026-02-20 12:09:04 +0530

    Added registerUser service

--- src/modules/userAuth/services/registerUser.service.js
+++ src/modules/userAuth/services/registerUser.service.js
@@ -2,5 +2,12 @@ import { AuthUser } from "../models/userAuth.model.js";
 import { ApiError } from "../../../utils/ApiError.js";
 
 const registerUser = async () =>{
-   
+    // check is user details are not empty
+    // user user is not already in db
+    // create user
+    // find user by userId 
+    // check user saved in db 
+    // return user
 }
+
+export {registerUser}
