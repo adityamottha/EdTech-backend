3ccf5d10f6c1047a24728dd50c3bcbaa7244c4bf
adityamottha <adityamottha5@gmail.com>
2026-02-20 21:10:56 +0530

    Added route for login

--- src/modules/userAuth/authUser.routes.js
+++ src/modules/userAuth/authUser.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { registerUserController } from "./authUser.controller.js";
+import { loginUserController, registerUserController } from "./authUser.controller.js";
 
 const router = Router();
 
 router.route("/register").post(registerUserController);
 
+// LOGIN ROUTER 
+router.route("/login").post(loginUserController)
 export default router;
