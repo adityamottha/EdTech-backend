7512bb8f3412b7278eda5ad741d5c356b1f47123
adityamottha <adityamottha5@gmail.com>
2026-02-23 20:28:23 +0530

    Completed teacherProfileSchema

---
+++ src/modules/userProfile/models/teacherProfle.model.js
@@ -0,0 +1,72 @@
+import {Schema,model} from "mongoose";
+
+const teacherProfileSchema = new Schema({
+    profileId:{
+        type:Schema.Types.ObjectId,
+        ref:"Profile",
+        required:true,
+        unique:true,
+        index:true
+    },
+
+    hiredDate:{
+        type:Date
+    },
+
+    specialization:{
+        type:String,
+        trim:true
+    },
+
+    qualification:[
+        {
+            degree:{
+                type:String,
+                trim:true
+            },
+            insititution:{
+                type:String,
+                trim:true
+            },
+            year:{
+                type:Date
+            }
+        }
+    ],
+
+    courseTought:[{
+        type:Schema.Types.ObjectId,
+        ref:"Course"
+    }],
+
+    rating:{
+        type:Number,
+        default:0
+    },
+
+    totalStudent:{
+        type:Number,
+        default:0
+    },
+
+    isApproved:{
+        type:Boolien,
+        default:false
+    },
+
+    approvedBy:{
+        type:Schema.Types.ObjectId,
+        ref:"Profile"
+    },
+
+    approvedDate:{
+        type:Date
+    },
+
+    bankDetails: {
+      type: String // store encrypted JSON string
+    }
+    
+},{timestamps:true});
+
+export const Teacher = model("Teacher",teacherProfileSchema);
