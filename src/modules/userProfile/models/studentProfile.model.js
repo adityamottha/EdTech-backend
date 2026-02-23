f1b131c146205bdeff67be535e729c2233e28f45
adityamottha <adityamottha5@gmail.com>
2026-02-23 19:43:08 +0530

    Completed student schema

---
+++ src/modules/userProfile/models/studentProfile.model.js
@@ -0,0 +1,52 @@
+import { Schema,model } from "mongoose";
+
+const studentProfileSchema = new Schema({
+    profileId:{
+        type:Schema.Types.ObjectId,
+        ref:"Profile",
+        index:true,
+        unique:true,
+        required:true
+    },
+
+    enrollmentDate:{
+        type:Date,
+        default:Date.now() // update date when user enroll course
+    },
+
+    currentLevel:{
+        type:String,
+        enum:["Beginner","Intermediate", "Advanced"],
+        default:"Beginner"
+    },
+
+    totalCourseEnrollment:{
+        Type:Number,
+        default:0
+    },
+
+
+    completedCourses: {
+      type: Number,
+      default: 0
+    },
+
+    averageGrade: {
+      type: Number,
+      default: 0
+    },
+
+    pointsEarned:{
+        type:Number,
+        default:0
+    },
+
+    badges: [
+      {
+        type: String
+      }
+    ]
+
+},{timestamps:true});
+
+export const Student = new model("Student",studentProfileSchema,)
