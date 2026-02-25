ddceaca5779ee779255a1e4dbc33a9b3988fa4cc
adityamottha <adityamottha5@gmail.com>
2026-02-25 22:06:00 +0530

    Injected multer middleware on router

--- src/modules/userProfile/Profile.routes.js
+++ src/modules/userProfile/Profile.routes.js
@@ -1,9 +1,18 @@
 import { Router } from "express";
 import { profileController } from "./controllers/profile.controller.js"
 import { verifyJWT } from "../../middlewares/verifyJWT.middleware.js";
+import { upload } from "../../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router.route("/common-profile").post(verifyJWT,profileController)
+router.route("/common-profile").post(
+    verifyJWT,
+    upload.fields([
+        {
+            name:"avatar",
+            maxCount:1
+        }
+    ]),
+    profileController)
 
 export default router;
