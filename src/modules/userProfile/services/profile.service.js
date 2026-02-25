549c1f479a3f7247319c0b83684a1001cf8e78d8
adityamottha <adityamottha5@gmail.com>
2026-02-25 21:47:45 +0530

    Completed profile service

--- src/modules/userProfile/services/profile.service.js
+++ src/modules/userProfile/services/profile.service.js
@@ -1,14 +1,67 @@
 import { ApiError } from "../../../utils/ApiError.js";
 import { Profile } from "../models/profile.model.js";
+import { uploadFileOnCloudinary } from "../../../utils/cloudinary.js";
 
-const profileService = async ({})=>{
- // check data is not empty
- // find profile
- // find user is existed 
- // check profile is not already submitted
- // create profile
-//  update fields 
+const profileService = async ({
+    userId,
+    firstName,
+    lastName,
+    phoneNumber,
+    avatar,
+    dateOfBirth,
+    bio,
+    address,
+    timezone,
+    socialLinks
+})=>{
+    // check data is not empty
+
+    if(!firstName?.trim()) throw new ApiError(400,"First-Name is required!");
+
+    if(!lastName?.trim()) throw new ApiError(400,"Last-Name is required!");
+
+    if(!phoneNumber?.trim()) throw new ApiError(400,"Phone-Number is required!");
+
+    if(!avatar?.trim()) throw new ApiError(400,"Avatar is required!");
+
+    if(!dateOfBirth?.trim()) throw new ApiError(400,"dateOfBirth is required!");
+
+    if(!bio?.trim()) throw new ApiError(400,"bio is required!");
+
+    if(!address?.trim()) throw new ApiError(400,"address is required!");
+
+    if(!timezone?.trim()) throw new ApiError(400,"timezone is required!");
+    
+    // find profile 
+    const existedPofile = await Profile.findOne({userId});
+
+    if(existedPofile){
+        throw new ApiError(408,"User Already have profile!");
+    }
+
+    // upload avatar on cloudinary
+    const uploadAvatar = await uploadFileOnCloudinary(avatar);
+    if(!uploadAvatar?.secure_url){
+        throw new ApiError(500,"Avatar failed to upload!");
+    };
+
+   // create profile
+   const profile = await Profile.create({
+    firstName,
+    lastName,
+    phoneNumber,
+    avatar:uploadAvatar.secure_url,
+    dateOfBirth,
+    bio,
+    address,
+    socialLinks,
+    timezone,
+    profileCompleted:true
+   });
+   
  // return
+
+ return profile;
 }
 
 export { profileService }
