c286a23be61e9826174ef73cc2795669ab0c0fe7
adityamottha <adityamottha5@gmail.com>
2026-02-25 18:06:16 +0530

    Added profile service and also algorithm

---
+++ src/modules/userProfile/services/profile.service.js
@@ -0,0 +1,14 @@
+import { ApiError } from "../../../utils/ApiError.js";
+import { Profile } from "../models/profile.model.js";
+
+const profileService = async ({})=>{
+ // check data is not empty
+ // find profile
+ // find user is existed 
+ // check profile is not already submitted
+ // create profile
+//  update fields 
+ // return
+}
+
+export { profileService }
