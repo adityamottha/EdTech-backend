4a31344929fa303cdbab7c16c31ce7294a434ff8
adityamottha <adityamottha5@gmail.com>
2026-02-26 14:49:59 +0530

    Updated controller

--- src/modules/userProfile/controllers/profile.controller.js
+++ src/modules/userProfile/controllers/profile.controller.js
@@ -1,16 +1,23 @@
 import { AsyncHandler } from "../../../utils/AsyncHandler.js";
 import { ApiResponse } from "../../../utils/ApiResponse.js"
 import { profileService } from "../services/profile.service.js";
+import { json } from "express";
 
 const profileController = AsyncHandler(async (req,res)=>{
     // find user_id from logged in user 
     const userId = req.user?._id;
 
     // find avatar from files 
-    const avatar = req.files?.avatar?.[0]?.path
+    const avatar = req.files?.avatar?.[0]?.path;
+
+    //address and social-links find and convert to json
+    const address = req.body.address? JSON.parse(req.body.address) : null;
+    const socialLinks = req.body.socialLinks? JSON.parse(req.body.socialLinks) : null;
 
     // get all data from req.body
-     const {firstName,lastName,phoneNumber,dateOfBirth,bio,address,timezone,socialLinks
+     const {
+        firstName,lastName,phoneNumber,
+        dateOfBirth,bio,timezone
     } = req.body;
 
     // get service function and pass-data,
