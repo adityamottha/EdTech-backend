b5857d829aadbe668f8a455ca94b91d675105ca6
adityamottha <adityamottha5@gmail.com>
2026-02-26 21:45:59 +0530

    Added teacherApplication Schema

--- src/modules/teacher/models/teacherApplication.model.js
+++ src/modules/teacher/models/teacherApplication.model.js
@@ -1,7 +1,43 @@
 import {Schema,model} from "mongoose";
 
 const teacherApplicationSchema = new Schema({
+    userId:{
+        type:Schema.Types.ObjectId,
+        ref:"AuthUser",
+        required:true,
+        unique:true,
+        index:true
+    },
 
+    specialization:{
+        type:String
+    },
+
+    qualification:[
+        {
+            degree:{
+                type:String
+            },
+            institution:{
+                type:String
+            },
+            year:{
+                type:Date
+            }
+        }
+    ],
+
+    approvalStatus:{
+        type:String,
+        enum:["Pending","Approved","Rejected"],
+        default:"Pending"
+    },
+
+    rejectedReason:{
+        type:String
+    }
+
+    
 },{timestamps:true});
 
 export const TeacherApplication = model("TeacherApplication",teacherApplicationSchema)
