d24f55cb3cba3708b246f95fe14d082414fe38f2
adityamottha <adityamottha5@gmail.com>
2026-02-27 14:12:53 +0530

    Added isProfileCompleted router

--- src/modules/teacher/teacher.routes.js
+++ src/modules/teacher/teacher.routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import { isProfileCompleted } from "../../middlewares/isProfileCompleted.middleware.js";
 import { verifyJWT } from "../../middlewares/verifyJWT.middleware.js";
 import { teacherApplicationController } from "./teacher.controller.js";
 
 const router = Router();
 
-router.route("/teacher-application").post(verifyJWT,teacherApplicationController);
+router.route("/teacher-application").post(
+    verifyJWT,
+    isProfileCompleted(),
+    teacherApplicationController
+);
 
 export default router;
