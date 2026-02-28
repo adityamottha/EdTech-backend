bcc492fedf051d0a2c9120562aa3230e0a3cf497
adityamottha <adityamottha5@gmail.com>
2026-02-28 11:45:36 +0530

    added getApplications service

---
+++ src/modules/admin/admin.service.js
@@ -0,0 +1,66 @@
+import { AuthUser } from "../auth/authUser.model.js";
+
+const getTeacherApplicationRequestService = async () =>{
+    return AuthUser.aggregate([
+        {
+            // only users who apply for teacher
+            $lookup:{
+                from: "TeacherApplication",
+                userId: "$_id",
+                pipeline:[
+                    {
+                        $match:{
+                            $expr:{
+                                $eq: ["$userId", "$$userId"]
+                            }
+                        }
+                    }
+                ],
+                as: "Applications"
+            }
+        },
+
+        // remove user who did not apply 
+        {
+            $match :{
+                teacherApplication : {$ne : [] }
+            }
+        },
+
+        // get profile data 
+       {
+        $lookup:{
+            from: "Profile",
+            let: {userId: "$_id"},
+            pipeline:[
+                {
+                    $match:{
+                        $expr:{
+                            $eq:["$userId" ,"$$userId"],
+                        }
+                    }
+                },
+                {
+                    $project:{
+                        firstName: 1,
+                        lastName: 1,
+                        phoneNumber: 1
+                    }
+                }
+            ],
+            as:"profile"
+        }
+       },
+
+       {
+        $project:{
+            email: 1,
+            role: 1,
+            teacherApplication: 1,
+            profile: { $arrayElemAt: ["$profile", 0] }
+        }
+       }
+    ])
+}
+
+export { getTeacherApplicationRequestService }
