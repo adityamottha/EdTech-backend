4aabfd85272960fd133c08c0ead8323e95cca2f5
adityamottha <adityamottha5@gmail.com>
2026-03-02 11:38:33 +0530

    Added boilerPlate and algorithms for approved teacher service

--- src/modules/admin/admin.service.js
+++ src/modules/admin/admin.service.js
@@ -1,66 +1,78 @@
 import { AuthUser } from "../auth/authUser.model.js";
 
 const getTeacherApplicationRequestService = async () =>{
-    return AuthUser.aggregate([
-        {
-            // only users who apply for teacher
-            $lookup:{
-                from: "TeacherApplication",
-                let: { userId: "$_id" },
-                pipeline:[
-                    {
-                        $match:{
-                            $expr:{
-                                $eq: ["$userId", "$$userId"]
-                            }
-                        }
-                    }
-                ],
-                as: "Applications"
+  return await AuthUser.aggregate([
+    {
+      // only users who applied for teacher
+      $lookup: {
+        from: "teacherapplications",
+        let: { userId: "$_id" },
+        pipeline: [
+          {
+            $match: {
+              $expr: {
+                $eq: ["$userId", "$$userId"]
+              }
             }
-        },
+          }
+        ],
+        as: "Applications"
+      }
+    },
 
-        // remove user who did not apply 
-        {
-            $match :{
-                teacherApplication : {$ne : [] }
+    // remove users who did not apply
+    {
+        $match:{
+            "Applications.0": { $exists: true }
+        }
+    },
+
+    // get profile data
+    {
+      $lookup: {
+        from: "profile",
+        let: { userId: "$_id" },
+        pipeline: [
+          {
+            $match: {
+              $expr: {
+                $eq: ["$userId", "$$userId"]
+              }
             }
-        },
+          },
+          {
+            $project: {
+              firstName: 1,
+              lastName: 1,
+              phoneNumber: 1
+            }
+          }
+        ],
+        as: "profile"
+      }
+    },
 
-        // get profile data 
-       {
-        $lookup:{
-            from: "Profile",
-            let: {userId: "$_id"},
-            pipeline:[
-                {
-                    $match:{
-                        $expr:{
-                            $eq:["$userId" ,"$$userId"],
-                        }
-                    }
-                },
-                {
-                    $project:{
-                        firstName: 1,
-                        lastName: 1,
-                        phoneNumber: 1
-                    }
-                }
-            ],
-            as:"profile"
-        }
-       },
+    {
+      $project: {
+        email: 1,
+        role: 1,
+        Applications: 1,
+        profile: { $arrayElemAt: ["$profile", 0] }
+      }
+    }
+  ]);
+};
 
-       {
-        $project:{
-            email: 1,
-            role: 1,
-            teacherApplication: 1,
-            profile: { $arrayElemAt: ["$profile", 0] }
-        }
-       }
-    ])
+// APPORVED TEACHER APPLICATATION STATUS
+
+const approvedTeacherService = async ({userId})=>{
+  // check userId is required
+  // find user by userId
+  // id user has already approved status throw error 
+  // if not  so approved status
+  // update fields like approvedAt 
+  // return 
+
 }
 
 export { getTeacherApplicationRequestService }
