a665cab869374c089c32b3a2c479f72519f6d1b6
adityamottha <adityamottha5@gmail.com>
2026-03-02 13:25:13 +0530

    Added controller for reject application

--- src/modules/admin/admin.routes.js
+++ src/modules/admin/admin.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { verifyJWT } from "../../middlewares/verifyJWT.middleware.js";
 import { authorizeRole } from "../../middlewares/authorizeRole.middleware.js"
-import { approvedTeacherController, getTeacherApplicationRequestController } from "./admin.controller.js";
+import { approvedTeacherController, getTeacherApplicationRequestController, rejectApplicationController } from "./admin.controller.js";
 
 const router = Router();
 
@@ -11,10 +11,19 @@ router.route("/applications").get(
     getTeacherApplicationRequestController
 );
 
+// APPROVED APPLICATION STATUS --------------------
 router.route("/approved-teacher").patch(
     verifyJWT,
     authorizeRole("Admin"),
     approvedTeacherController
 );
+
+// REJECT APPLICATION STATUS ---------------------
+router.route("/reject-application").patch(
+    verifyJWT,
+    authorizeRole("Admin"),
+    rejectApplicationController
+)
+
 
 export default router;
