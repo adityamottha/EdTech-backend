da0dc9594896cc50b6a769456501c131ea5d3fd7
adityamottha <adityamottha5@gmail.com>
2026-06-10 18:46:46 +0530

    Completed controller for create course

---
+++ src/modules/courses/controllers/course.controller.js
@@ -0,0 +1,38 @@
+import { ApiResponse } from "../../../utils/ApiResponse.js"
+import { createCourseService } from "../services/course.service.js"
+import { AsyncHandler } from "../../../utils/AsyncHandler.js"
+
+
+// ==================== CREATE COURSE ==================
+const createCourseController = AsyncHandler(async (req,res)=>{
+
+    // get data from body 
+    const { title,
+      description,
+      thumbnail,
+      price,
+      duration,
+      level,
+      language,
+    } = req.body
+
+    // get userId from auth middleware
+    const instructorId = req.user?._id;
+
+    // get service function and pass parameters
+    const course = await createCourseService({title,
+      description,
+      thumbnail,
+      price,
+      duration,
+      level,
+      language,
+      instructorId
+    });
+
+    return res.status(200).json(
+        new ApiResponse(200,course,"Course created successully!")
+    );
+});
+
+export { createCourseController}
