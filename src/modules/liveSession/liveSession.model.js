6e34d95a0aa5d2b66a0f22651486198577214914
adityamottha <adityamottha5@gmail.com>
2026-06-20 17:21:15 +0530

    added some fields in schema

--- src/modules/liveSession/liveSession.model.js
+++ src/modules/liveSession/liveSession.model.js
@@ -15,18 +15,26 @@ const liveSessionSchema = new Schema({
 
     title:{
         type:String,
-        required:true
+        required:true,
+        trim:true
     },
 
      description:{
         type:String,
-        required:true
+        required:true,
+        trim:true
      },
 
      durationMinutes:{
         type:Number,
         default:0,
         required:true
+     },
+
+     meetingLink:{
+        type:String,
+        required:true,
+        trim:true
      },
 
      sessionStatus:{
