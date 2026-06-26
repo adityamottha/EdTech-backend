ec58acee87a2be5548e71108c68e20d8e7cad80d
adityamottha <adityamottha5@gmail.com>
2026-06-26 17:23:19 +0530

    Added controller for getNotification

---
+++ src/modules/notification/notification.controller.js
@@ -0,0 +1,16 @@
+import { AsyncHandler } from "../../utils/AsyncHandler.js";
+import { ApiResponse } from "../../utils/ApiResponse.js";
+import { getAllNotificationsService } from "./notification.service.js";
+
+// ================ GET USER ALL NOTOFICATION ==============
+export const getAllNotificationController = AsyncHandler(async (req,res) =>{
+
+    // find userId from req.user
+    const userId = req.user?._id;
+
+    // call service worker 
+    const notification = await getAllNotificationsService(userId);
+
+    // response
+    return  notification;
+});
