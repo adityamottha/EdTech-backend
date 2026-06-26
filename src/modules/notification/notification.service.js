68e1d87796e37fda4fc7ca2ae26a7b70a1e71c81
adityamottha <adityamottha5@gmail.com>
2026-06-26 17:19:19 +0530

    Completed get notification and regoister notifiaction service

--- src/modules/notification/notification.service.js
+++ src/modules/notification/notification.service.js
@@ -1,8 +1,8 @@
 import { Notification } from "./notification.model.js";
 import { ApiError } from "../../utils/ApiError.js";
 
-/* notification service >>>
-1) notify user when register
+/* 
+notification service >>>
 2) notify on course enrollment
 3) notify when user apply for teacher
 4) notify when user approved for teacher
@@ -12,4 +12,61 @@ import { ApiError } from "../../utils/ApiError.js";
 8) notify students when teacher completed live session
 9) notify students when teacher cancelled the live session
 10) notify when payment success
-11) notify when payment failed */
+11) notify when payment failed
+ */
+
+//  =============== NOTIFY USER WHEN REGISTER ===========
+const registerWelcomeNotificationService = async (userId)=>{
+    // check everything is given 
+    if(!userId) throw new ApiError(
+        400,
+        "UserId required For registerNotification!"
+    );
+
+    // check is userAlready has register welcome notification
+    const existedNotification = await Notification.findOne({
+      userId,
+      type:"WELCOME"
+    });
+
+    if(existedNotification) return;
+
+    // create notification
+    const notification = await Notification.create(
+        {
+            userId,
+            title:"Welcome to addicPiano",
+            message:"Explore our best courses and start your learning journey!",
+            type:"WELCOME",
+            priority:"LOW"
+        }
+    )
+
+    // return
+    return notification;
+};
+
+
+// ==================== GET ALL NOTIFICATIONS =================
+
+const getAllNotificationsService = async (userId) =>{
+
+    // check userid is required
+    if(!userId){
+        throw new ApiError(
+            400,
+            "Userid is required!"
+        )
+    };
+
+    // find notification by userId 
+    const findNotification = await Notification.findById(userId)
+
+    // return
+    return findNotification
+}
+
+export {
+    registerWelcomeNotificationService,
+    getAllNotificationsService
+};
